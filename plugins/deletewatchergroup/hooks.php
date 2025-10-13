5db90558a492cad28ce98c7db69fb71a5119f3a4
Алан <bulgakov.alan@list.ru>
2025-10-13 18:08:57 +0300

    бизнес правило работает!!! но частично
    если есть уже группа в базе данных и я сохраняю тикет(обновляю) то у меня группу удаляется так как есть связь в базе данных в таблице glpi_groups_tickets но при остальных условиях плагин пока что работать не будет

--- plugins/deletewatchergroup/hooks.php
+++ plugins/deletewatchergroup/hooks.php
@@ -1,9 +1,6 @@
 <?php
 //Hooks.php
 
-use CommonITILActor;
-use Ticket;
-
 if (!defined('GLPI_ROOT')) {
     die("Извини. У тебя недостаточно прав для просмотра");
 }
@@ -13,10 +10,15 @@ include_once(GLPI_ROOT . '/plugins/deletewatchergroup/inc/log.php');
 function plugin_deletewatchergroup_executeActions($data) {
     plugin_deletewatchergroup_log("Функция успешно активировалась и получила нужный параметр data");
 
+    $counterNotMatch = 0;
+    $counterNeedMatch = 0;
+
     $output = $data['output'] ?? [];
     $action = $data['action'] ?? [];
     $input = $data['input'] ?? [];
     $params = $data['params'] ?? [];
+
+    global $DB;
 
     if (!$action instanceof RuleAction) {
         plugin_deletewatchergroup_log("Ошибка: действия нет в объекте RuleAction");
@@ -33,48 +35,110 @@ function plugin_deletewatchergroup_executeActions($data) {
     if ($action->fields['action_type'] === 'delete_observers' 
         && $action->fields['field'] === '_groups_id_observer'){
 
-        plugin_deletewatchergroup_log("прошло условие что action_type = delete_observers 
-            и field = _groups_id_observer" );
+        plugin_deletewatchergroup_log(  "прошло условие что action_type = delete_observers 
+                                         и field = _groups_id_observer" );
 
-        $value = (int)$action->fields['value'];
+        $value=(int)$action->fields["value"];
 
-        plugin_deletewatchergroup_log("переменная value = $value");
+        $iteratorGroup = $DB->request(   [
+                                    'FROM' => 'glpi_groups',
+                                    'WHERE' => ['id' => $value],
+                                    'LIMIT' => 1]);
 
-        if (!isset($input['id']) || $input['id'] <= 0) {
+        $rowGroup = $iteratorGroup->current(); 
+
+        plugin_deletewatchergroup_log("RowGroup после current(): " . var_export($rowGroup, true));
+
+        if (!$rowGroup) {
+            plugin_deletewatchergroup_log("Группа $value не найдена — пропускаем");
             return $output;
         }
-
-        $ticket = new Ticket();
-
-        plugin_deletewatchergroup_log("получение нового обьекта Ticket" . print_r($ticket, true));
-
-        if (!$ticket->getFromDB($input['id'])){
-            return $output;
-        }
-
-
-        plugin_deletewatchergroup_log("вывод лога после того как обьект тикета загрузили из БД" 
-                                        . print_r($ticket, true));
-
-
-        // Получаем текущие группы наблюдателей из таблицы glpi_groups_tickets
-
+        
         if (isset($output["_groups_id_observer_deleted"])) {
             foreach ( $output["_groups_id_observer_deleted"] as $key=>$array)
             {
-                plugin_deletewatchergroup_log("Элемент с ключом $key: " . print_r($arr, true));
+                plugin_deletewatchergroup_log("Элемент с ключом $key: " . print_r($array, true));
+
+                if ((int)$array["items_id"] === (int)$action->fields["value"]){
+                    plugin_deletewatchergroup_log("сравнение прошло успешно следовательно я все правильно написал xD");
+                    plugin_deletewatchergroup_log($output["_groups_id_observer_deleted"][$key]["id"] 
+                                                . "    " 
+                                                . $action->fields['value']);
+
+                    $counterNotMatch = $counterNotMatch + 1;
+
+                } else {
+                    plugin_deletewatchergroup_log("нифига тут не равно! ");
+
+                    $counterNeedMatch = $counterNeedMatch + 1;
+
+                }   
             }
 
-            plugin_deletewatchergroup_log("вывожу массив _groups_id_observer_deleted по ключам" 
-                                            . $output["_groups_id_observer_deleted"][$key]);
-        }
 
-            // plugin_deletewatchergroup_log("Group $value not found in observer groups.");
+
+            if ($counterNotMatch > 0) {
+
+                plugin_deletewatchergroup_log("добавлять ничего не нужно!");
+
+
+            } elseif ($counterNeedMatch >=1 || $counterNotMatch === 0) {
+
+                $idGroupTicket = 0;
+
+                // не терять! где я проверяю существует ли тикет (обновление или создание нового)
+                if (isset($input['id']) || $input['id'] > 0) {
+
+                    plugin_deletewatchergroup_log   ("айдишник тикета и его тип: " . (int)$input['id'] . "тип: " . gettype((int)$input['id'])
+                                                    . "айдишник группы: "  . $value . "тип: " . gettype($value));
+
+                    $iteratorTicket = $DB->request([
+                                    'FROM' => 'glpi_groups_tickets',
+                                    'WHERE' =>  [
+                                                'tickets_id' => (int)$input['id'],
+                                                'groups_id' => (int)$value,
+                                                'type' => (int)3
+                                                ],
+                                    'LIMIT' => 1]);
+
+                    $rowTicket = $iteratorTicket->current(); 
+
+                    if (!$rowTicket) {
+                        plugin_deletewatchergroup_log("связь между тикетами и группами не найдена — пропускаем");
+                        return $output;
+                    }
+
+                    plugin_deletewatchergroup_log(  "запрос к бд выдал такой результат: " . var_export($rowTicket, true));
+                    $idGroupTicket = $rowTicket['id'];
+                }
+                else {
+
+                }
+
+                $entry = [
+                        'id' => $idGroupTicket,
+                        'items_id' => $value,
+                        'itemtype' => 'Group',
+                        'text' => $rowGroup['name'],
+                        'title' => $rowGroup['completename'] ?? $rowGroup['name']
+                        ];
+
+                plugin_deletewatchergroup_log("нужно добавить пометку в массив на удаление группы" . print_r($entry, true));
+
+                $output["_groups_id_observer_deleted"][] = $entry;
+
+
+                plugin_deletewatchergroup_log("вот и переменная которая выходит в конце" . print_r($output, true));
+                return $output;
+            }
+        }
         
     } else {
-        // plugin_deletewatchergroup_log("Skip: not delete_observers or wrong field.");
+        plugin_deletewatchergroup_log("НЕ прошло условие что action_type = delete_observers 
+            и field = _groups_id_observer");
     }
 
-    plugin_deletewatchergroup_log("Возвращаемый output: " . print_r($output, true));
+
+    plugin_deletewatchergroup_log("вот и переменная которая выходит в конце" . print_r($output, true));
     return $output;
 }
